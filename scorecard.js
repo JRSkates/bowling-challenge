ccb4f7ac9ac3f8a3c3dd22407a0e13c94ea17156
JRSkates <skatesproduction@gmail.com>
2023-05-29 16:55:32 +0100

    Core functionality test driven

---
+++ scorecard.js
@@ -0,0 +1,37 @@
+class ScoreCard {
+  constructor() {
+    this.frames = [];
+    this.count = 10;
+  }
+
+  addFrame(pointOne, pointTwo) {
+    this.count -= 1;
+    if (this.frames.length === 0) {
+      this.frames.push([pointOne, pointTwo]);
+    } else if (this.frames[this.frames.length - 1][0] === 10) {
+      let sum = pointOne + pointTwo;
+      let lastFrame = this.frames[this.frames.length - 1][0]
+      this.frames[this.frames.length - 1][0] = sum + lastFrame
+      this.frames.push([pointOne, pointTwo]);
+    } else if (this.frames[this.frames.length - 1].reduce((total, amount) => total + amount) === 10) {
+      let lastFrame = this.frames[this.frames.length - 1][0]
+      this.frames[this.frames.length - 1][0] = pointOne + lastFrame
+      this.frames.push([pointOne, pointTwo]);
+    } else {
+      this.frames.push([pointOne, pointTwo]);
+    }
+    console.log(this.frames);
+    console.log(this.count);
+    console.log(this.frames[this.frames.length - 1][0]);
+  }
+
+  calculateScore() {
+    if(this.frames.length === 0) return 0;
+    let result = this.frames.flat().reduce((total, amount) => total + amount);
+    return result;
+  }
+
+}
+
+module.exports = ScoreCard
+
---
+++ tests/scorecard.test.js
@@ -0,0 +1,46 @@
+const ScoreCard = require('../scorecard');
+
+describe('ScoreCard', () => {
+  it('returns 0 when calculating score of a new game', () => {
+    let scorecard = new ScoreCard();
+    expect(scorecard.calculateScore()).toEqual(0);
+  });
+
+  it('returns the total after adding 1 frame', () => {
+    let scorecard = new ScoreCard();
+    scorecard.addFrame(3, 4);
+    expect(scorecard.calculateScore()).toEqual(7);
+  });
+
+  it ('returns the total after adding 3 frames', () => {
+    let scorecard = new ScoreCard();
+    scorecard.addFrame(3, 4);
+    scorecard.addFrame(5, 4)
+    scorecard.addFrame(2, 1);
+    expect(scorecard.calculateScore()).toEqual(19);
+  });
+
+  it ('correctly calculates a strike', () => {
+    let scorecard = new ScoreCard();
+    scorecard.addFrame(10, 0);
+    scorecard.addFrame(3, 2);
+    expect(scorecard.calculateScore()).toEqual(20);
+  });
+
+  it ('correctly calculates multiple strikes', () => {
+    let scorecard = new ScoreCard();
+    scorecard.addFrame(10, 0);
+    scorecard.addFrame(3, 2);
+    scorecard.addFrame(5, 2);
+    scorecard.addFrame(10, 0);
+    scorecard.addFrame(3, 6);
+    expect(scorecard.calculateScore()).toEqual(55);
+  });
+
+  it('correctly calculates a spare', () => {
+    let scorecard = new ScoreCard();
+    scorecard.addFrame(8, 2);
+    scorecard.addFrame(3, 2);
+    expect(scorecard.calculateScore()).toEqual(18)
+  });
+});
